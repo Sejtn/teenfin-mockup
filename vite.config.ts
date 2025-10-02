ae08a129372086031141e87f510f7fb421034c0f
Tarar1k <47252567+Sejtn@users.noreply.github.com>
2025-10-02 15:15:55 +0200

    ok
    ano

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/teenfin-mockup/', // <- dôležité pre GitHub Pages
   plugins: [react()],
-  base: '/teenfin-mockup/',
 })
